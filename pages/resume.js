fe253b01d60c7e1481319fff07bcd2461cb066b2
Chiawen Chen <golopot@gmail.com>
2019-07-10 21:44:20 +0800

    Second commit

--- pages/resume.js
+++ pages/resume.js
@@ -1,9 +1,29 @@
 import Head from 'next/head';
 import './resume.css';
 
-function Home() {
+/**
+ * @param {{
+ *   title: string,
+ *   date: string,
+ *   body: any,
+ * }} props
+ */
+function ExperienceItem(props) {
+  const {title, date, body} = props;
   return (
-    <main class="content">
+    <div className="experience__item">
+      <div>
+        <span className="experience__date">{date}</span>{' '}
+        <span className="experience__title">{title}</span>
+      </div>
+      <div className="experience__body">{body}</div>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <main className="content">
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -11,28 +31,28 @@ function Home() {
         <meta name="Description" content="Resume of Chiawen Chen" />
         <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
-        <title>陳嘉文 履歷</title>
+        <title>Resume - Chiawen Chen 陳嘉文</title>
       </Head>
 
-      <section class="intro">
-        <div class="portrait">
+      <section className="intro">
+        <div className="portrait">
           <img src="/static/portrait.jpg" alt="portrait" />
         </div>
         <h1>陳嘉文 Chiawen Chen</h1>
         <div>
           <p>
             Web application developer, frontend, backend, and db. Specialties: node.js, javascript,
-            react, vue, express. I maintain eslint-plugin-jsdoc.
+            react, vue, express. Has been active on Github.
           </p>
           <p>
             <a href="https://github.com/golopot">github</a>
             <br />
             <a href="https://stackoverflow.com/users/3290397/golopot">stackoveflow</a>
           </p>
-          <p>
+          <div>
             <div>golopot@gmail.com</div>
             <div>Taipei, Taiwan</div>
-          </p>
+          </div>
         </div>
       </section>
 
@@ -45,32 +65,53 @@ function Home() {
       </section>
 
       <section>
-        <h3>Experience</h3>
-        <div class="experience">
-          <div>
-            <span>2007-09 - 2010-06</span> 建國高級中學 畢業
-          </div>
-          <div>
-            <span>2010-10 - 2014-07</span> 清華大學數學系 肄業
-          </div>
-          <div>
-            <span>2015-11 - 2016-04</span> 龍雲數位科技, Intelligent vendor machine - Full-stack
-          </div>
-          <div>
-            <span>2016-04 - 2018-02</span> ---
-          </div>
-          <div>
-            <span>2018-02 - 2018-05 </span> 台大商業智慧實驗室, Sportswear Market Intelligence
-            System - Full-stack, vue, express
-          </div>
-          <div>
-            <span>2018-09 - 2019-06 </span> 快樂電視, Live streaming platform - Full-stack, vue,
-            express
-          </div>
+        <h3>Education</h3>
+
+        <div className="experience">
+          <ExperienceItem date="2007-09 - 2010-06" title="Taipei Municipal Jianguo High School" />
+
+          <ExperienceItem
+            date="2010-10 - 2014-07"
+            title="Attended National Tsing Hua University, major in
+            mathematics."
+          />
         </div>
       </section>
 
-      <section class="works">
+      <section>
+        <h3>Experience</h3>
+        <div className="experience">
+          <ExperienceItem
+            date="2015-11 - 2016-04"
+            title="Transtep technology corporation, Programmer"
+            body={<>Develop Intelligent Vendor Machine in JavaScript. </>}
+          />
+
+          <ExperienceItem
+            date="2018-02 - 2018-05"
+            title="Bright Insight lab, Full stack programmer"
+            body={
+              <>
+                Develop SMIS system (Sportswear Market Intelligence System) in vue and express and
+                MongoDB.
+              </>
+            }
+          />
+
+          <ExperienceItem
+            date="2018-09 - present"
+            title="HappyTV, Engineer"
+            body={
+              <>
+                Develop the Live streaming website in Vue, Node.js and MySQL. Responsible for chat
+                server and firebase cloud messaging services.
+              </>
+            }
+          />
+        </div>
+      </section>
+
+      <section className="works">
         <h3>Projects</h3>
 
         <section>
@@ -91,9 +132,23 @@ function Home() {
           </h4>
           <p>Build a cat tree online.</p>
         </section>
+
+        <section>
+          <h4>
+            <span>Eslint-plugin-jsdoc</span>{' '}
+            <a href="https://github.com/gajus/eslint-plugin-jsdoc">[link]</a>
+          </h4>
+          <p>I am one of the maintainers.</p>
+        </section>
+
+        <section>
+          <h4>
+            <span>Eslint-plugin-react</span>{' '}
+            <a href="https://github.com/yannickcr/eslint-plugin-react">[link]</a>
+          </h4>
+          <p>I am a frequent contributor.</p>
+        </section>
       </section>
     </main>
   );
 }
-
-export default Home;
